05a044e933ffdf38fcafc75599f7fcf6069a3ea8
RODRIGO <silveira.sc@gmail.com>
2018-04-25 10:27:56 -0300

    primeiro commit

---
+++ src/pages/login/login.ts
@@ -0,0 +1,49 @@
+import { UsersProvider } from './../../providers/users/users';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+  model: User;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private toast: ToastController, private userProvider: UsersProvider) {
+    this.model = new User();
+    this.model.email = 'silveira.sc@gmail.com';
+    this.model.password = '123456';
+  }
+
+  login() {
+    this.userProvider.login(this.model.email, this.model.password)
+      .then((result: any) => {
+
+        console.log(result);
+
+        if (result.success){
+          this.toast.create({ message: 'Usuário logado com sucesso. Token: ' + result.token, position: 'botton', duration: 3000 }).present();
+        }
+        else {
+          this.toast.create({ message: result.msg, position: 'botton', duration: 3000 }).present();
+        }
+        
+        
+        
+
+        //Salvar o token no Ionic Storage para usar em futuras requisições.
+        //Redirecionar o usuario para outra tela usando o navCtrl
+        //this.navCtrl.pop();
+        //this.navCtrl.setRoot()
+      })
+      .catch((error: any) => {
+        this.toast.create({ message: 'Erro ao efetuar login. Erro: ' + error.error, position: 'botton', duration: 3000 }).present();
+      });
+  }
+}
+
+export class User {
+  email: string;
+  password: string;
+}
